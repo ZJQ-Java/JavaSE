81c67e135d50b2e11b2c73d79f1ec1adcef2fbdf
zhangjinqiu03 <zhangjinqiu03@58.com>
2020-11-05 22:40:48 +0800

    thread Join Yield

---
+++ HelloWorld/src/main/java/ThreadTest/YieldTest.java
@@ -0,0 +1,19 @@
+package ThreadTest;
+//礼让，看cpu心情
+public class YieldTest implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+"----begin----");
+        Thread.yield();
+        System.out.println(Thread.currentThread().getName()+"----end----");
+    }
+
+    public static void main(String[] args) {
+        Thread t = new Thread(new YieldTest(),"a");
+        Thread t1 = new Thread(new YieldTest(),"b");
+        t.start();
+        t1.start();
+
+    }
+}
