2ceb86c8091e240a58299b7285a030275a343536
zhangjinqiu03 <zhangjinqiu03@58.com>
2020-12-25 18:01:15 +0800

    daemon

---
+++ HelloWorld/src/main/java/ThreadTest/DaemonDemo.java
@@ -0,0 +1,25 @@
+package ThreadTest;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.Queue;
+
+public class DaemonDemo {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            while (true) {
+                try {
+                    System.out.println("========daemon==========");
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        //开启守护，自动结束
+        thread.setDaemon(true);
+        thread.start();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("=========main============");
+        }
+    }
+}
